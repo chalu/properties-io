50c8bb4cf71fa51924c1e3b3f900b413867abc46
Charles Odili <chaluwa@gmail.com>
2020-04-10 09:21:06 +0100

    v1 release

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const propertiesReader = require('properties-reader');
 
+const run = async () => {
+  try {
+    const file = core.getInput('path');
+    const properties = propertiesReader(file);
 
-// most @actions toolkit packages have async methods
-async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    let values = [];
+    const readFrom = core.getInput('read-from');
+    if (readFrom) {
+      values = readFrom
+        .trim()
+        .split(/\s+/)
+        .reduce((props, field) => {
+          props.push(properties.get(field));
+          return props;
+        }, []);
+    }
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
+    core.setOutput('values', values);
+  } catch (error) {
     core.setFailed(error.message);
   }
-}
+};
 
-run()
+run();
